0a4f7cf79116368dc909ef21d860a4056ecdd3a6
sonalan <sinan@onalan.net>
2025-09-06 15:42:06 -0700

    fix: add state for searchTerm for better search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Advocate from "../types/advocate"
 export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -17,10 +18,8 @@ export default function Home() {
     });
   }, []);
 
-  const onChange = (e) => {
-    const searchTerm = e.target.value;
-
-    document.getElementById("search-term").innerHTML = searchTerm;
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value)
 
     console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
@@ -30,7 +29,7 @@ export default function Home() {
         advocate.city.includes(searchTerm) ||
         advocate.degree.includes(searchTerm) ||
         advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience === searchTerm
+        advocate.yearsOfExperience.toString().includes(searchTerm)
       );
     });
 
@@ -39,6 +38,7 @@ export default function Home() {
 
   const onClick = () => {
     console.log(advocates);
+    setSearchTerm('');
     setFilteredAdvocates(advocates);
   };
 
@@ -50,9 +50,9 @@ export default function Home() {
       <div>
         <p>Search</p>
         <p>
-          Searching for: <span id="search-term"></span>
+          Searching for: <span id="search-term">{searchTerm}</span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
+        <input style={{ border: "1px solid black" }} onChange={onChange} value={searchTerm} />
         <button onClick={onClick}>Reset Search</button>
       </div>
       <br />
