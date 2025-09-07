1dafd8607132eb1e48725e7467cbb9ba1501fb1b
sonalan <sinan@onalan.net>
2025-09-07 01:33:33 -0700

    feat: adds searchParams for search capability

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -2,7 +2,14 @@ import db from "../../../db";
 import { advocates } from "../../../db/schema";
 // import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const searchTerm = searchParams.get('search_term');
+  const pageSize = parseInt(searchParams.get('page_size') || '10');
+  const page = parseInt(searchParams.get('page') || '1');
+
+  const offset = (page -1) * pageSize
+  
   // Uncomment this line to use a database
   const data = await db.select().from(advocates);
 
